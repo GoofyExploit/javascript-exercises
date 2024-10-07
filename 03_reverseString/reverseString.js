f9b820a4864dc074c2d278fdf6c61cda8cd823b8
GoofyExploit <groverlucky406@gmail.com>
2024-10-07 12:59:14 +0530

    03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split('')
+    let result = []
+    let firstLetter
+    for (const letters of wordArray) {
+        result.unshift(letters)
+    }
+    return result.join('')
 };
 
 // Do not edit below this line
