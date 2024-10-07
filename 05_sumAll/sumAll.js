4f5450921e309013e4f94570e0208ffc20c7be69
GoofyExploit <groverlucky406@gmail.com>
2024-10-07 16:37:53 +0530

    05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    // Check for invalid inputs
+    if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number' ||
+        !Number.isInteger(firstNumber) || !Number.isInteger(lastNumber) ||
+        firstNumber < 0 || lastNumber < 0) {
+        return 'ERROR';
+    }
 
-};
+    // Swap if firstNumber is greater than lastNumber
+    if (firstNumber > lastNumber) {
+        [firstNumber, lastNumber] = [lastNumber, firstNumber];
+    }
+
+    let sum = 0;
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
