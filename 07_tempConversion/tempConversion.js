95ba731e0bc2ef8facc4adf46509b37ab91dc0b2
GoofyExploit <groverlucky406@gmail.com>
2024-10-07 17:12:28 +0530

    07 cmpleted

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let c = (5/9) * (temperature - 32)
+  if(Number.isInteger(c)){
+    return c
+  }else{
+    return parseFloat(c.toFixed(1))
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let f = (9/5) * temperature + 32
+  if(Number.isInteger(f)){
+    return f
+  }else{
+    return parseFloat(f.toFixed(1))
+  }
 };
 
 // Do not edit below this line
